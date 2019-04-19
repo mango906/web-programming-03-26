a4b8ae938db45ab347e44d308cc7ac94b3d7d234
mango906 <minkb01@dgsw.hs.kr>
2019-04-19 21:03:36 +0900

    mysql -> h2

--- src/main/java/kr/hs/dgsw/web_0326/Domain/Comment.java
+++ src/main/java/kr/hs/dgsw/web_0326/Domain/Comment.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Comment {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
